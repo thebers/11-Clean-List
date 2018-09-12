c7c788af51bb926a01ae8d86ac5e33ce30034e60
Erik WIlder <erikkurtu@gmail.com>
2018-09-12 19:28:32 -0400

    localforage & template

--- js/app.js
+++ js/app.js
@@ -1,36 +1,63 @@
+var listData = [];
+function showData() {
+  localforage.getItem('benapp').then(function(benappValue) {
+    if (typeof benappValue !== 'undefined' && benappValue !== null) {
+      listData = benappValue;
+    }
+    $("#dataVal").html(listData);
+    $("#checklist").html(_.template($("#listTemplate").html(), listData));
+  }).catch(function(err) {
+    console.log(err);
+  });
+}
+
+var listData = [
+  {
+    name: "one",
+    checked: true
+  },
+  {
+    name: "true",
+    checked: false
+  }
+];
+
 $(document).ready(function(){
+  showData();
+  $("#addButton").click(function() {
+    var itemInputValue = $("#itemToAdd").val();
 
-    $("#addButton").click(function() {
-      var itemInputValue = $("#itemToAdd").val();
-      $("#checklist").append("<li class='list-group-item listItem'>"+
-        "<button type='button' class='btn btn-danger deleteButton'>remove</button>"+'  '+
-        itemInputValue+"</li>");
-        //localforage.setItem(1, itemInputValue, storedValue);
-      $("#itemToAdd").val("");
-    });
+    $("#checklist").append("");
 
-
-    $("#resetList").click(function() {
-      $("#checklist").empty();
-      $("#itemToAdd").val("");
-    });
-
-    $('body').on('click', '.listItem', function() {
-      if ((this).css("background-color"=="#666666")) {
-          $(this).css("background-color", "#FFFFFF");
-      }
-      else {
-        $(this).css("background-color", "#666666");
-        $(this).insertAfter($('#checklist').children().last());
-      }  
-    });
-
-    $('body').on('click', '.deleteButton', function() {
-      $(this).parent().remove();
-      e.stopPropagation();
+    $("#itemToAdd").val("");
+    listData.push(itemInputValue);
+    localforage.setItem('benapp', listData).then(function (value) {
+      showData();
+    }).catch(function(err) {
+        console.log(err);
     });
   });
 
+  $("#resetList").click(function() {
+    $("#checklist").empty();
+    $("#itemToAdd").val("");
+  });
 
-  // var listItems;
-  // $("#listResults").html(_.template($("#listTemplate").html(), listItems));
+  $("body").on("click", ".listItem", function() {
+    if ($(this).hasClass("grayBG")) {
+      $(this).removeClass("grayBG");
+    } else {
+      $(this).addClass("grayBG");
+      $(this).insertAfter($('#checklist').children().last());
+    }  
+  });
+
+  $("body").on("click", ".deleteButton", function() {
+    $(this).parent().remove();
+    e.stopPropagation();
+  });
+
+});
+
+// var listItems;
+// $("#listResults").html(_.template($("#listTemplate").html(), listItems));
